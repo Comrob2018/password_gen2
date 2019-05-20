6f3b7f40a20b920467e57679a02eb6c65cedd2e5
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2019-05-20 16:15:01 -0500

    testing cmd line args

--- main.py
+++ main.py
@@ -3,26 +3,76 @@ import sys
 import getopt
 csv.register_dialect('pDialect', delimiter='|')
 
-def multi_pass(amount,length):
-    global pwordlist
-    pwordlist=[[]]
-    count=0
-    pwordfile=open('Passwords.csv', 'w')    
-    while count<amount:
-        pword2=''
-        getPurpose()
-        getUserName()
-        pword2+=purpose+','+uName+','
-        pword_gen(length)
-        pword2+=pword
-        pwordlist.append(pword2.split(','))
-        count+=1
-    writer=csv.writer(pwordfile, dialect='pDialect')
-    for row in pwordlist:
-        writer.writerow(row)
-    pwordfile.close()
-    return "Passwords have been generated, check the file labeled Passwords.csv"
+chars='aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ000111222333444555666777888999__..--@@##$$!!&%&**%'
 
+def pword_gen(length):
+    global pword
+    pword=''
+    while len(pword)<length:
+        if specials=='y'and numbers=='y':
+            pword+=chars[randint(0,101)]
+        elif specials!='y' and numbers=='y':
+            pword+=chars[randint(0,81)]
+        else:
+            pword+=chars[randint(0,51)]
+    return pword
+
+def multi_pass(amount,length):
+    pwordlist=[]
+    count=0
+    pwordfile=open(filename, 'w')    
+    while count<amount:
+        pword_gen(length)
+        pwordlist.append(pword)
+        count+=1
+    pwordfile.write(str(pwordlist))
+    pwordfile.close()
+    return "Passwords have been generated, check the file labeled" + filename
   
 def main(argv):
+    global length
+    length = ''
+    global amount
+    amount = ''
+    global numbers
+    numbers = ''
+    global specials
+    specials = ''
+    global filename
+    filename = ''
+    
+    try:
+        opts, args = getopt.getopt(argv, "ha:f:l:ns, ["amount=","filename=","length=","numbers","specials"])
+    except getopt.GetoptError:
+        print('''passgen.py -a ## <amount of passwords to generate> 
+                            -f <output filename> 
+                            -h <help message> 
+                            -l ## <character length of password> 
+                            -n <password requires numbers> 
+                            -s <password requires special characters>''')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('''passgen.py -a ## <amount of passwords to generate> 
+                                -f <output filename> 
+                                -h <help message> 
+                                -l ## <character length of password> 
+                                -n <password requires numbers> 
+                                -s <password requires special characters>''')
+            sys.exit()
+        elif opt in ('-a', '--amount'):
+            amount = int(arg)
+        elif opt in ('-f', '--filename'):
+            filename = arg
+        elif opt in ('-l', '--length'):
+            length = int(arg)
+        elif opt in ('-n', '--numbers'):
+            numbers = 'y'
+        elif opt in ('s', '--specials'):
+            specials = 'y'
+                                   
+    print(multi_pass(amount,length))
+                                   
+if __name__=='__main__':
+    main(sys.argv[1:])
     
